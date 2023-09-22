0cd2bd11dc4b0dbb1e0b79833170cccccd6d3047
tyler <91284723+fatteryacid@users.noreply.github.com>
2023-09-22 08:04:20 -0700

    Added try except block as temp catch for new html returns

--- app/Nosy.py
+++ app/Nosy.py
@@ -1,14 +1,15 @@
 from bs4 import BeautifulSoup
 from datetime import datetime
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 from webdriver_manager.firefox import GeckoDriverManager
 
-import time
 
 class Nosy:
     def __init__(self, config):
@@ -65,31 +66,41 @@ class Nosy:
         # This is not efficient, fix later
         return base_url + f"make={car_metadata['make']}&" +f"model={car_metadata['model']}&" + f"zip={_zip}&" + f"localization={localization}&" + f"title={title}"
 
-    def convert_result(self):
+    def convert_result(self, car_metadata):
         result_list = self.cached_data.find_all("li", class_="result-list-item")
 
         for i in result_list:
-            obj = {
-                "year_make_model": i.find("a", class_="listing-link source-link").get_text().strip(),
-                "price":  i.find("div", class_="badge__label label--price").get_text().strip(),
-                "mileage": i.find("span", class_="mileage").get_text().strip(),
-                "listing_location": i.find("span", class_="city").get_text().strip(),
-                "listing_date": i.find("span", class_="date").get_text().strip(),
-                "listing_id": i.section["data-listing-id"].strip(),
-                "fetch_ts": self.timestamp.strftime("%Y-%m-%d %H:%M:%S")
-            }
-
-            self.output_list.append(obj)
+            try:
+                # need to investigate why some pieces of data are NOT coming in with prices
+                # and handle accordingly
+                obj = {
+                    "year_string": i.find("a", class_="listing-link source-link").get_text().strip(),
+                    "make": car_metadata["make"],
+                    "model": car_metadata["model"],
+                    "price":  i.find("div", class_="badge__label label--price").get_text().strip(),
+                    "mileage": i.find("span", class_="mileage").get_text().strip(),
+                    "listing_location": i.find("span", class_="city").get_text().strip(),
+                    "listing_date": i.find("span", class_="date").get_text().strip(),
+                    "listing_id": i.section["data-listing-id"].strip(),
+                    "listing_url": i.a["href"].strip(),
+                    "fetch_ts": self.timestamp.strftime("%Y-%m-%d %H:%M:%S")
+                }
+            except:
+                error = open(f"./error_log/error_log_{self.timestamp.strftime('%Y-%m-%d')}.txt", "w")
+                error.write(i.prettify())
+                pass
+            finally:
+                self.output_list.append(obj)
 
     def fetch_data(self):
         for car in self.search_parameters["cars"]:
             self.driver.get(self.build_url(car))
             self.wait_for_conditions()
-            time.sleep(10)  # for testing the theory that these wait conditions aren't waiting long enough
+            time.sleep(15)  # for testing the theory that these wait conditions aren't waiting long enough
             self.cached_data = BeautifulSoup(self.driver.page_source, "html.parser")
             self.driver.close()
             self.timestamp = datetime.now()
-            self.convert_result()
+            self.convert_result(car)
 
 
     def get_data(self):
