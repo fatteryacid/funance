b49800af55d32ca1e5141d0a0b7d6dd7c7ffcaf8
tyler <91284723+fatteryacid@users.noreply.github.com>
2023-09-30 21:09:42 -0700

    Change database write strategy

--- app/DataHandler.py
+++ app/DataHandler.py
@@ -4,9 +4,11 @@ import os
 import psycopg2 as pg
 
 class DataHandler:
-    def __init__(self, configuration):
+    def __init__(self, configuration, logger):
         self.database_initialization_status = configuration["initialized"]
         self.database_metadata = configuration["connection_metadata"]
+        self.transformation_scripts = configuration["transform"]
+        self.load_scripts = configuration["load"]
         self.init_directory = "./database/initialize/"
         self.elt_directory = "./database/elt/"
         self.raw_data_directory = "./raw_data/"
@@ -19,20 +21,63 @@ class DataHandler:
         )
         self.cursor = self.connector.cursor()
 
-        self.logger = Logger()
-        self.logger.open_file()
+        self.logger = logger
         self.logger.write_to_file("Database connection successfully started.")
 
-    def execute_sequential_scripts(self, directory):
-        for script in os.listdir(directory):
-            f = os.path.join(directory, script)
+    def commit_changes(self):
+        """Explicitly commits all changes to database."""
+        
+        self.connector.commit()
+        self.logger.write_to_file("Successfully commited block.")
+        return True
 
-            if os.path.isfile(f):
-                j = open(f, "r").read()
-                self.cursor.execute(j)
-                self.connector.commit()
-                print(f"successfully executed script: {f}")
-                self.logger.write_to_file(f"Successfully excecuted script: '{f}'.")
+    def insert_data(self, car_object):
+        """Begins a transaction block of INSERT INTO statements for each individual listing. Function DOES NOT commit changes."""
+
+        command = f'''
+        INSERT INTO staging_raw_data (make, model, _year, vin, location_zipcode, _location, mileage, price, _url, listing_id, listing_date, details, fetch_ts)
+        VALUES(
+            '{car_object.make}',
+            '{car_object.model}',
+            '{car_object.year}',
+            '{car_object.vin}',
+            '{car_object.location_zipcode}',
+            '{car_object.location}',
+            '{car_object.mileage}',
+            '{car_object.price}',
+            '{car_object.url}',
+            '{car_object.listing_id}',
+            '{car_object.listing_date}',
+            '{car_object.details.replace("'", "")}',    -- cursed
+            '{car_object.fetch_ts}'
+        );
+        '''
+
+        try:
+            self.cursor.execute(command)
+        except Exception as e:
+            self.logger.write_to_file(f"Encountered error sending INSERT INTO to database: {e}.")
+
+    def run_sql_scripts(self, directory, script_list):
+        """Runs all transformation scripts in order"""
+
+        for i in script_list:
+            f = open(directory + i).read()
+            self.cursor.execute(f)
+
+    def process_raw_data(self):
+        self.run_sql_scripts(self.elt_directory, self.transformation_scripts)
+        self.logger.write_to_file(f"Successfully ran transformation scripts.")
+
+    def load_to_production(self):
+        self.run_sql_scripts(self.elt_directory, self.load_scripts)
+        self.logger.write_to_file(f"Successfully ran load scripts.")
+
+    def close(self):
+        self.connector.close()
+        self.logger.write_to_file("Successfully performed transformation and load scripts, closing connection.")
+
+
 
     def update_config(self):
         self.logger.write_to_file("Instructed to update database configuration.")
@@ -45,18 +90,29 @@ class DataHandler:
         }
         out_config = json.dumps(compiled_config, indent=4)
 
-        with open("./data_config.json", "w") as outfile:
+        with open("./configs/user_data_config.json", "w") as outfile:
             outfile.write(out_config)
 
         self.logger.write_to_file("Successfully updated configuration.")
 
+# TODO: Re-evaluate initialize_database() and reset_database() functions
     def initialize_database(self):
         if self.database_initialization_status is True:
-            self.logger.write_to_file(f"Database already initialized.")
+            #self.logger.write_to_file(f"Database already initialized.")
             return
 
         self.logger.write_to_file("Database not initialized. Creating new database.")
-        self.execute_sequential_scripts(self.init_directory)
+
+        for script in os.listdir(self.init_directory):
+            f = os.path.join(self.init_directory, script)
+
+            if os.path.isfile(f):
+                j = open(f, "r").read()
+                self.cursor.execute(j)
+                self.connector.commit()
+                print(f"successfully executed script: {f}")
+                self.logger.write_to_file(f"Successfully excecuted script: '{f}'.")
+
         self.database_initialization_status = True
         self.update_config()
         self.logger.write_to_file("Successfully initialized database.")
@@ -81,36 +137,14 @@ class DataHandler:
                 self.logger.write_to_file("User denied databsae reset.")
                 break
 
-    def insert_data(self, data_filename):
-        command = f'''
-            COPY staging_raw_data(
-                empty_set,
-                year_string,
-                make, 
-                model, 
-                price,
-                mileage,
-                listing_location,
-                listing_date,
-                listing_id,
-                listing_url,
-                fetch_ts
-            )
-            FROM '{os.path.join(os.path.abspath(self.raw_data_directory), data_filename)}'
-            DELIMITER ','
-            CSV HEADER;
-        '''
-
-        self.cursor.execute(command)
-        self.connector.commit()
-        self.logger.write_to_file(f"Successfully ingested {data_filename}.")
-
     def perform_transformations(self):
+        # TODO: maybe separate out each script into its own function call
+        # TODO: maybe house load / transform scripts in config file
         scripts = [
             'process_and_insert_staging_data.sql',
-            'remove_from_raw.sql',
-            'insert_into_production.sql',
-            'remove_from_processed.sql'
+            #'remove_from_raw.sql',
+            'insert_into_production.sql'
+            #'remove_from_processed.sql'
         ]
 
         for i in scripts:
@@ -118,8 +152,10 @@ class DataHandler:
 
             f = open(path, "r").read()
             self.cursor.execute(f)
-            self.connector.commit()
             self.logger.write_to_file(f"Successfully ran script '{i}'.")
+
+
+
 
 
 
